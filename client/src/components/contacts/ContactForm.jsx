e877513b1d46d1a7a849c5384cb4ff10fa96f709
Tom Shamp <T.shamp7@gmail.com>
2020-10-22 17:39:12 -0700

    add edit contact functionality

--- client/src/components/contacts/ContactForm.jsx
+++ client/src/components/contacts/ContactForm.jsx
@@ -1,8 +1,23 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
+  const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
+        name: "",
+        email: "",
+        phone: "",
+        type: "personal",
+      });
+    }
+  }, [contactContext, current]);
+
   const [contact, setContact] = useState({
     name: "",
     email: "",
@@ -18,18 +33,28 @@ const ContactForm = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    contactContext.addContact(contact);
-    setContact({
-      name: "",
-      email: "",
-      phone: "",
-      type: "personal",
-    });
+    if (current === null) {
+      addContact(contact);
+      setContact({
+        name: "",
+        email: "",
+        phone: "",
+        type: "personal",
+      });
+    } else {
+      updateContact(contact);
+    }
+  };
+
+  const clearAll = () => {
+    clearCurrent();
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <h2 className='text-primary'>Add Contact</h2>
+      <h2 className='text-primary'>
+        {current === null ? "Add Contact" : "Edit Contact"}
+      </h2>
       <input
         type='text'
         placeholder='name'
@@ -70,11 +95,18 @@ const ContactForm = () => {
       <div>
         <input
           type='submit'
-          value='Add Contact'
+          value={current === null ? "Add Contact" : "Update Contact"}
           className='btn btn-primary btn-block'
           onChange={onChange}
         />
       </div>
+      {current && (
+        <div>
+          <button className='btn btn-light btn-block' onClick={clearAll}>
+            Clear
+          </button>
+        </div>
+      )}
     </form>
   );
 };
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -10,6 +10,23 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
     case DELETE_CONTACT:
       return {
         ...state,
