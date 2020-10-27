f2f25d67fcebf2c77e489fbe660423607dd2c14b
Thomas Shamp <T.shamp7@gmail.com>
2020-10-27 13:26:35 -0700

    Implement alert context and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,36 @@ import About from "./components/pages/About";
 import Navbar from "./components/layout/Navbar";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import AlertState from "./context/alert/AlertState";
+import Alerts from "./components/layout/Alerts";
+import setAuthToken from "./utils/setAuthToken";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment className='App'>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home}></Route>
-                <Route exact path='/about' component={About}></Route>
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment className="App">
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/about" component={About}></Route>
+                  <Route exact path="/register" component={Register}></Route>
+                  <Route exact path="/login" component={Login}></Route>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
