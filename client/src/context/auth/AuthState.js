985ba048c9167b3dd2f803be44e2c4778410f265
Thomas Shamp <T.shamp7@gmail.com>
2020-10-30 12:00:33 -0700

    add login user functionality

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -38,8 +38,6 @@ const AuthState = (props) => {
         type: USER_LOADED,
         payload: res.data,
       });
-
-      loadUser();
     } catch (err) {
       dispatch({ type: AUTH_ERROR });
     }
@@ -59,6 +57,8 @@ const AuthState = (props) => {
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
@@ -67,8 +67,28 @@ const AuthState = (props) => {
     }
   };
   // Login User
-  const login = () => {
-    console.log("login user");
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
   };
   // Logout
   const logout = () => {
