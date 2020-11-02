e427c77549f57b83d42a3c76027efa01c6bb05b8
Thomas Shamp <T.shamp7@gmail.com>
2020-11-02 10:21:39 -0800

    Added files for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -8,9 +9,18 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.send({ msg: "Welcome to the Contact Keeper API" })
-);
+// app.get("/", (req, res) =>
+//   res.send({ msg: "Welcome to the Contact Keeper API" })
+// );
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Define routes
 app.use("/api/users", require("./routes/users"));
